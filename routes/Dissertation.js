ee82318f6a287f119b28b99aada8f2658897b9ae
ronniepiku <ronniepiku1@hotmail.co.uk>
2023-10-16 18:26:41 +0100

    add dissertation

--- routes/Dissertation.js
+++ routes/Dissertation.js
@@ -6,7 +6,7 @@ var router = express.Router();
 module.exports = function(db, logger){
 
 	router.get('/', function(req, res, next) {
-		res.sendFile(path.join(config.server.appDirectory, config.server.publicDirectory, config.server.CV));
+		res.sendFile(path.join(config.server.appDirectory, config.server.publicDirectory, config.server.Dissertation));
 	});
 	
 	return router;
