176e26de2b2217ba785366f2bb5a21b13903b994
ronniepiku <ronniepiku1@hotmail.co.uk>
2023-10-17 15:50:34 +0100

    .

--- app.js
+++ app.js
@@ -9,35 +9,9 @@ function create(db) {
 	app = express();
     
 	// Configure paths
-	app.use('/', require(path.join(__dirname, config.server.routesDirectory, 'index'))(db, logger));
 	app.use('/', require(path.join(__dirname, config.server.routesDirectory, 'shortlinks'))(db, logger));
-	app.use('/schedule', require(path.join(__dirname, config.server.routesDirectory, 'schedule'))(db, logger));
-	app.use('/mail', require(path.join(__dirname, config.server.routesDirectory, 'mail'))(db, logger));
-	app.use('/analytics', require(path.join(__dirname, config.server.routesDirectory, 'analytics'))(db, logger));
-	app.use('/analytics2', require(path.join(__dirname, config.server.routesDirectory, 'analytics2'))(db, logger));
-	app.use('/map', require(path.join(__dirname, config.server.routesDirectory, 'map'))(db, logger));
-	app.use('/api/viewdata', require(path.join(__dirname, config.server.routesDirectory, 'api/viewdata'))(db, logger));
-	app.use('/api/viewdataanalysis', require(path.join(__dirname, config.server.routesDirectory, 'api/viewdataanalysis'))(db, logger));
 	app.use('/CV', require(path.join(__dirname, config.server.routesDirectory, 'CV'))(db, logger));
     app.use('/Dissertation', require(path.join(__dirname, config.server.routesDirectory, 'Dissertation'))(db, logger));
-	app.use('/api/wiki', require(path.join(__dirname, config.server.routesDirectory, 'api/wiki'))(db, logger));
-	app.use('/api/collage', require(path.join(__dirname, config.server.routesDirectory, 'api/collage'))(db, logger));
-	app.use('/api/ping', require(path.join(__dirname, config.server.routesDirectory, 'api/ping'))(db, logger));
-    
-	app.use('/dashboard', require(path.join(__dirname, config.server.routesDirectory, 'dashboard/index'))(db, logger));
-	app.use('/dashboard/collage', require(path.join(__dirname, config.server.routesDirectory, 'dashboard/collage'))(db, logger));
-	app.use('/dashboard/gas', require(path.join(__dirname, config.server.routesDirectory, 'dashboard/gas'))(db, logger));
-	app.use('/misc', require(path.join(__dirname, config.server.miscDirectory))(db, logger));
-	app.use('/ping', require(path.join(__dirname, config.server.routesDirectory, 'ping'))(db, logger));
-
-    app.use('/files', function(req, res, next) {
-        if (fs.lstatSync(path.join(__dirname, config.server.publicDirectory, decodeURI(req.url))).isFile()) {
-            res.redirect(req.url);
-        }
-        else {
-            serveIndex(path.join(__dirname, config.server.publicDirectory), { icons: true })(req, res, next);
-        }
-    });
 	app.use(express.static(path.join(__dirname, config.server.publicDirectory)));
 	
 	// Catch 404 and forward to error handler
